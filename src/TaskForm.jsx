c6608e01f2449585a4153be94f5a1f00db9f0744
Hilal38 <hhilalkocak@gmail.com>
2024-05-06 22:12:22 +0300

    Add tasks

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -1,50 +1,62 @@
-export default function TaskForm() {
-    return <>
+import { useState } from "react"
 
-    <form className="row g-3">
-  <div className="col-md-6">
-    <label htmlFor="inputEmail4" className="form-label">Email</label>
-    <input type="email" className="form-control" id="inputEmail4" />
-  </div>
-  <div className="col-md-6">
-    <label htmlFor="inputPassword4" className="form-label">Password</label>
-    <input type="password" className="form-control"  id="inputPassword4"/>
-  </div>
-  <div className="col-12">
-    <label htmlFor="inputAddress" className="form-label">Address</label>
-    <input type="text" className="form-control" id="inputAddress"  placeholder="1234 Main St"/>
-  </div>
-  <div className="col-12">
-    <label htmlFor="inputAddress2" className="form-label">Address 2</label>
-    <input type="text" className="form-control" id="inputAddress2"  placeholder="Apartment, studio, or floor"/>
-  </div>
-  <div className="col-md-6">
-    <label htmlFor="inputCity" className="form-label">City</label>
-    <input type="text" className="form-control" id="inputCity" />
-  </div>
-  <div className="col-md-4">
-    <label htmlFor="inputState" className="form-label">State</label>
-    <select id="inputState" className="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div className="col-md-2">
-    <label htmlFor="inputZip" className="form-label">Zip</label>
-    <input type="text" className="form-control" id="inputZip" />
-  </div>
-  <div className="col-12">
-    <div className="form-check">
-      <input className="form-check-input" type="checkbox"  id="gridCheck"/>
-      <label className="form-check-label" htmlFor="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <div className="col-12">
-    <button type="submit" className="btn btn-primary">Sign in</button>
-  </div>
-</form>
-    
+export default function TaskForm() {
+    const emptyForm = {
+        task: "",
+        priority: false
+    }
+    const [formData, setFormData] = useState(emptyForm)
+    const [tasks, setTasks]  = useState([])
+    function handleInputChange(event) {
+        
+
+        setFormData(prev => {
+            return {
+                ...prev,
+                [event.target.name]: event.target.type === "checkbox" ? event.target.checked : event.target.value
+            }
+        })
+
+        
+    }
+    function handleFormSubmit(event) {
+        
+        event.preventDefault()
+        console.log("form data", formData)
+        if(formData.task.length > 3){
+            setTasks(
+                prev => [formData, ...prev ]
+            )
+            setFormData(emptyForm)
+            event.target.reset()
+        }
+    }
+
+    return <>
+    <ul>
+        {tasks.map((item, index) => <li key={index}>{item.task}</li>)}
+    </ul>
+        <form onSubmit={handleFormSubmit}>
+        <div className="row mb-3">
+            <label htmlFor="task" className="col-sm-2 col-form-label">Task</label>
+            <div className="col-sm-10">
+            <input type="text" className="form-control" id="task" name="task" onChange={handleInputChange}/>
+            </div>
+        </div>
+
+
+        <div className="row mb-3">
+            <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox" id="priority" name= "priority" onChange={handleInputChange}/>
+                <label className="form-check-label" htmlFor="priority">
+                High Priority
+                </label>
+            </div>
+            </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Save</button>
+        </form>
+            
     </>   
 }
